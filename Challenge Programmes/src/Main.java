bb8625b8d8079617fe6db5ee3946c4e3b5440648
Ani811625 <aniruddhas387@gmail.com>
2024-07-14 10:35:00 +0530

    First commit

---
+++ Challenge Programmes/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Good Morning");
+        //Pattern Problems
+        //Do not use loop.Use simple print statements.
+
+        //*****
+        //****
+        //***
+        //**
+        //*
+
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
